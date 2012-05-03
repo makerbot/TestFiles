e84b8ec48e67dc645c127bfae463ff19b6a00598
Joe Sadusk <joseph.sadusk@makerbot.com>
2012-05-03 16:41:17 -0400

    Initial attempt at stats collection, if only the stats were accurate

--- scripts/slice_analyzer.py
+++ scripts/slice_analyzer.py
@@ -6,7 +6,7 @@ import re
 from pysvg.structure import svg
 from pysvg.shape import line
 from pysvg.builders import StyleBuilder
-
+from math import sqrt
 
 layer_begin = re.compile('^\((<layer>|Slice) [\d.]+.*\)$')
 gcode_move = re.compile('G1 (X(-?\d+\.?\d*))? (Y(-?\d+\.?\d*))? (Z(-?\d+\.?\d*))? (F(\d+\.?\d*))?')
@@ -22,7 +22,6 @@ min_y = 0
 
 def layer_filename(layer_num):
     return dump_dir+'/layer_'+str(layer_num)+'.svg'
-
 
 class GCode(object):
     (MOVE, EXTRUDE) = range(2)
@@ -89,6 +88,12 @@ class Movement(Coord):
     def layer_change(self):
         return self.z > 0
 
+    def distance(self):
+        return sqrt(self.x**2 + self.y**2 + self.z**2)
+
+    def duration(self):
+        return self.distance() / (self.speed / 60)
+
 class Position(Coord):
     def move(self, newpos, speed):
         movement = Movement(self, newpos, speed)
@@ -114,25 +119,71 @@ class Extruder(object):
     move_style.setStrokeWidth(.3 * factor)
     move_style.setStroke('blue')
 
-    def __init__(self):
+    def __init__(self, stats):
         self.extruding = False
+        self.stats = stats
 
-    def isExtruding(self):
+    def is_extruding(self):
         return self.extruding
+
+    def set_extruding(self, val):
+        if self.extruding != val:
+            stats.switch_extruder()
+            self.extruding = val
 
     def from_gcode(self, gcode_line):
         code = GCode.code_from_line(gcode_line)
         if code in ("M102", "M103"):
-            self.extruding = False
+            self.set_extruding(False)
         elif code == "M101":
-            self.extruding = True
+            self.set_extruding(True)
 
     def getStyle(self):
         if self.extruding:
             return self.extrude_style
         else:
             return self.move_style
+
+class StatsCollector(object):
+    def __init__(self, layernum):
+        self.new_layer(layernum)
         
+    def add_move(self, move):
+        self.move_times.append(move.duration())
+
+    def add_extrude(self, move):
+        self.extrude_times.append(move.duration())
+
+    def switch_extruder(self):
+        self.extruder_switches += 1
+
+    def stats_filename(self):
+        return dump_dir + "/layer_" + str(self.layernum) + "_stats.txt"
+
+    def move_time(self):
+        return sum(self.move_times)
+
+    def extrude_time(self):
+        return sum(self.extrude_times)
+
+    def total_time(self):
+        return self.move_time() + self.extrude_time()
+
+    def write(self):
+        fh = open(self.stats_filename(), "w")
+        fh.write("Move time: " + str(self.move_time()) + "\n")
+        fh.write("Number of moves: " + str(len(self.move_times)) + "\n")
+        fh.write("Extrude time: " + str(self.extrude_time()) + "\n")
+        fh.write("Number of extrudes: " + str(len(self.extrude_times)) + "\n")
+        fh.write("Total time: " + str(self.total_time()) + "\n")
+        fh.write("Extruder switches: " + str(self.extruder_switches))
+        fh.close()
+
+    def new_layer(self, layernum):
+        self.move_times = []
+        self.extrude_times = []
+        self.layernum = layernum
+        self.extruder_switches = 0
 
 ################################################################################
 
@@ -158,8 +209,8 @@ cur = Position()
 out = None
 layer_num = None
 
-
-extruder = Extruder()
+stats = StatsCollector(0)
+extruder = Extruder(stats)
 
 for gcode_line in gcode_fh.readlines():
     gcode_line = gcode_line.rstrip()
@@ -179,7 +230,7 @@ for gcode_line in gcode_fh.readlines():
         if match is not None:
             layer_num = 0
             out = svg("Layer "+str(layer_num))
-
+            stats = StatsCollector(layer_num)
         continue
 
     if movement is None:
@@ -189,11 +240,19 @@ for gcode_line in gcode_fh.readlines():
         out.save(layer_filename(layer_num))
         layer_num += 1
         out = svg("Layer "+str(layer_num))
+
+        stats.write()
+        stats.new_layer(layer_num)
     else:
         myline = movement.toSVG(extruder.getStyle())
         if myline is None:
             raise Exception("line is being returned as None")
         out.addElement(myline)
+
+        if extruder.is_extruding():
+            stats.add_extrude(movement)
+        else:
+            stats.add_move(movement)
 
 out.save(layer_filename(layer_num))
     
